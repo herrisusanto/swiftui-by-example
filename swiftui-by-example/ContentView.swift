3c90b372df1248000dc36c825b8a49008d0b60c8
Heri Susanto <herisusanto.app@gmail.com>
2024-02-01 12:42:24 +0700

    Initial Commit

---
+++ swiftui-by-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-by-example
+//
+//  Created by loratech on 01/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-by-example/swiftui_by_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_by_exampleApp.swift
+//  swiftui-by-example
+//
+//  Created by loratech on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_by_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
