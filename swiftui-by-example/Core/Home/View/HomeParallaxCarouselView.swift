79750d0c267b8c62320281820e1b24e9d1c1911d
Heri Susanto <herisusanto.app@gmail.com>
2024-02-09 17:01:26 +0700

    Add Parallax carousel view

---
+++ swiftui-by-example/Core/Home/View/HomeParallaxCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeParallaxCarouselView.swift
+//  swiftui-by-example
+//
+//  Created by loratech on 09/02/24.
+//
+
+import SwiftUI
+
+struct HomeParallaxCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeParallaxCarouselView()
+}
---
+++ swiftui-by-example/Core/Model/TripCard.swift
@@ -0,0 +1,8 @@
+//
+//  TripCard.swift
+//  swiftui-by-example
+//
+//  Created by loratech on 09/02/24.
+//
+
+import Foundation
